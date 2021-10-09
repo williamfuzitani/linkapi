48b2f5e10f88f6674f22e538b16a88d649bc7127
William Fuzitani <williamfuzitani@gmail.com>
2021-10-08 21:21:31 -0300

    Add config files

---
+++ src/config/bling.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: `https://bling.com.br/Api/v2/`,
+  token: "13c65d40994e68fc2d0b14179843dd4ea5faac0d2d08510c8fca8184f252eee5e9f68ce9",
+};
---
+++ src/config/pipedrive.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: `https://william2.pipedrive.com/api/v1/`,
+  token: "be5dbe311b691503633917b7ccf748c7fc9c645f",
+};
