ebf91c81d105671a01a442ba6bbb39f9c972a6f0
William Fuzitani <williamfuzitani@gmail.com>
2021-10-11 00:15:47 -0300

    add config files

--- src/config/pipedrive.js
+++ src/config/pipedrive.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: `https://william2.pipedrive.com/api/v1/`,
-  token: "be5dbe311b691503633917b7ccf748c7fc9c645f",
+  url: process.env.PIPEDRIVE_URL,
+  key: process.env.PIPEDRIVE_KEY,
 };
